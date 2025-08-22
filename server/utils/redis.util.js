4c74767700a9574d6aa4e9c7644a6d2b8a0d009c
Sahil Vaishnav🍊 <vaishnavsahil98@gmail.com>
2025-08-22 16:01:59 +0530

    debug redis 2

--- server/utils/redis.util.js
+++ server/utils/redis.util.js
@@ -1,7 +1,13 @@
 import { Redis } from "ioredis";
 
 console.log("REDIS_URL =>", process.env.REDIS_URL);
-const redis = new Redis(process.env.REDIS_URL);
+// const redis = new Redis(process.env.REDIS_URL);
+const redis = new Redis({
+    host: process.env.REDISHOST,
+    port: process.env.REDISPORT,
+    password: process.env.REDISPASSWORD,
+    username: process.env.REDISUSER || "default",
+});
 
 redis.on("connect", () => {
     console.log("[Redis connected]");
