96640b55e90c07106c806dc6b3aced3aca0571ec
Sahil Vaishnav🍊 <vaishnavsahil98@gmail.com>
2025-08-22 18:09:00 +0530

    redis fix

--- server/utils/redis.util.js
+++ server/utils/redis.util.js
@@ -1,13 +1,13 @@
 import { Redis } from "ioredis";
 
 console.log("REDIS_URL =>", process.env.REDIS_URL);
-// const redis = new Redis(process.env.REDIS_URL);
-const redis = new Redis({
-    host: process.env.REDISHOST,
-    port: process.env.REDISPORT,
-    password: process.env.REDISPASSWORD,
-    username: process.env.REDISUSER || "default",
-});
+const redis = new Redis(process.env.REDIS_URL, + "?family=0");
+// const redis = new Redis({
+//     host: process.env.REDISHOST,
+//     port: process.env.REDISPORT,
+//     password: process.env.REDISPASSWORD,
+//     username: process.env.REDISUSER || "default",
+// });
 
 redis.on("connect", () => {
     console.log("[Redis connected]");
